96a980f9380c0a8d9cc48a831448728d2db95690
Kirill2690 <kirillmuravev2690@gmail.com>
2022-10-23 11:20:33 +0300

    fix desctop

--- src/Main.tsx
+++ src/Main.tsx
@@ -2,11 +2,12 @@
 import React from 'react';
 import {Pages} from "./components/pages/Pages";
 import {Header} from "./components/header/Header";
+import s from './Main.module.css'
 
 
 export const Main = () => {
     return (
-        <div>
+        <div className={s.main_container}>
             <Header/>
             <Pages/>
         </div>
